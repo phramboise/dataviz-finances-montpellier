6d812f99e07638d0d2442def39fb49253e1727d0
Thomas Parisot <oncletom@users.noreply.github.com>
2019-02-14 12:05:44 +0100

    Utilisation de Intl.NumberFormat pour afficher les montants M14

--- src/shared/js/components/MoneyAmount.js
+++ src/shared/js/components/MoneyAmount.js
@@ -1,16 +1,25 @@
 import React from 'react';
 
 const MILLION = 1000000;
+const THIN_SPACE = ' ';
+
+const underMillion = new Intl.NumberFormat('fr-FR', {
+    style: 'currency',
+    currency: 'EUR',
+    minimumFractionDigits: 2,
+    maximumFractionDigits: 2
+});
+const aboveMillion = new Intl.NumberFormat('fr-FR', {
+    minimumFractionDigits: 1,
+    maximumFractionDigits: 1
+});
 
 export function makeAmountString(amount){
-    return (
-        amount < 100000 ?
-    `${(amount).toFixed(0)}€` :
-    `${(amount/MILLION).toFixed(1)} M€`
-    )
-    .replace('.', ',');
+    return amount < 100000 ?
+        underMillion.format(amount) :
+        aboveMillion.format(amount / MILLION) + THIN_SPACE + 'M€';
 }
 
 export default ({amount}) => {
-    return React.createElement('span', {className: 'money-amount'}, makeAmountString(amount));
+    return <span className="money-amount">{makeAmountString(amount)}</span>;
 };
