4d8c5a02f0663f832578f649a01c133a46f4b90d
Thomas Parisot <oncletom@users.noreply.github.com>
2019-03-04 17:24:48 +0100

    Fusion des familles

--- src/shared/js/components/BubbleChartCluster.js
+++ src/shared/js/components/BubbleChartCluster.js
@@ -6,39 +6,66 @@ import { hierarchicalByFunction } from "../finance/memoized.js";
 import MoneyAmount from "./MoneyAmount.js";
 import BubbleChartNode from "./BubbleChartNode.js";
 
+const mergeHierarchies = (...hierarchies) => {
+    const levels = new Map();
+
+    hierarchies.forEach(children => {
+        children.forEach(({id, label, total, children}) => {
+            const levelId = id.split('-').pop();
+
+            if (!levels.has(levelId)) {
+                levels.set(levelId, {
+                    id: levelId,
+                    label,
+                    total,
+                    children: [],
+                });
+            }
+
+            levels.get(levelId).children = levels.get(levelId).children.concat(children.toArray());
+        });
+    });
+
+    return Array.from(levels.values());
+};
+
 export default class BubbleChartCluster extends React.Component {
     constructor(props) {
         super(props);
 
         this.state = {
-            r: false,
-            d: true,
-            f: true,
-            i: true,
+            R: false,
+            D: true,
+            F: true,
+            I: true,
             activeElement: null
         }
     }
 
     render() {
         const {m52Instruction} = this.props;
+        const {R, D, F, I} = this.state;
 
         if (!m52Instruction) {
             return null;
         }
 
-        const families = hierarchicalByFunction(m52Instruction, DF).children.toArray();
+        const families = mergeHierarchies(
+            hierarchicalByFunction(m52Instruction, DF).children,
+            hierarchicalByFunction(m52Instruction, DI).children,
+        );
 
         return (<div className="bubble-chart-cluster">
             <nav className="legend-list legend-list--interactive">
                 <ul>
                     <li>
                         <label>
-                            <input type="radio" name="rd" value="r" defaultChecked={false} /> recettes
+                            <input type="radio" name="rd" value="R" defaultChecked={R} /> recettes
                         </label>
                     </li>
                     <li>
                         <label>
-                            <input type="radio" name="rd" value="d" defaultChecked={true} /> dépenses
+                            <input type="radio" name="rd" value="D" defaultChecked={D} /> dépenses
                         </label>
                     </li>
                 </ul>
@@ -46,18 +73,19 @@ export default class BubbleChartCluster extends React.Component {
                 <ul>
                     <li>
                         <label>
-                            <input type="checkbox" name="fi" value="f" defaultChecked={true} /> fonctionnement
+                            <input type="checkbox" name="fi" value="F" defaultChecked={F} /> fonctionnement
                         </label>
                     </li>
                     <li>
                         <label>
-                            <input type="checkbox" name="fi" value="i" defaultChecked={true} /> investissement
+                            <input type="checkbox" name="fi" value="I" defaultChecked={I} /> investissement
                         </label>
                     </li>
                 </ul>
             </nav>
 
-            {families.map((node) => (<BubbleChartNode key={node.id} node={node} />))}
+            {families.map((node) => (<BubbleChartNode key={node.id}
+                                                      node={node} />))}
         </div>)
     }
 }
--- src/shared/js/components/BubbleChartNode.js
+++ src/shared/js/components/BubbleChartNode.js
@@ -4,12 +4,18 @@ import {pack, hierarchy} from 'd3-hierarchy';
 
 import MoneyAmount from "./MoneyAmount.js";
 
-export default function BubbleChartNode ({node, width=400, height=400}) {
-    const {total, label} = node;
-    const children = node.children.toArray();
+/*
+TODO remove the onClick event as soon as https://github.com/visionmedia/page.js/issues/532
+     is solved
+ */
+
+export default function BubbleChartNode ({node}) {
+    const {total, label, children} = node;
+    const WIDTH = 400;
+    const HEIGHT = 400;
 
     const bubbles = pack({children})
-        .size([width, height])
+        .size([WIDTH, HEIGHT])
         .padding(30);
     const nodes = hierarchy({children}).sum(d => d.total);
 
@@ -19,13 +25,15 @@ export default function BubbleChartNode ({node, width=400, height=400}) {
             <span>{label}</span>
         </figurelegend>
 
-        <svg viewBox={`0 0 ${width} ${height}`}>
-            {bubbles(nodes).descendants().filter(d => !d.children).map((circle) => (
-            <g transform={`translate(${circle.x}, ${circle.y})`}>
-                <circle r={circle.r}
-                        className="rdfi-D"
-                        aria-label={circle.data.label} />
-                <text>{circle.data.label}</text>
+        <svg viewBox={`0 0 ${WIDTH} ${HEIGHT}`} xmlnsXlink="http://www.w3.org/1999/xlink">
+            {bubbles(nodes).descendants().filter(d => !d.children).map(({r, x, y, data}) => (
+            <g transform={`translate(${x}, ${y})`}>
+                <a xlinkHref={`#!/finance-details/${data.id}`} onClick={(e) => e.preventDefault() && page.redirect(`/finance-details/${page.id}`)}>
+                    <circle r={r}
+                            className={`rdfi-${data.rdfi[1]}`}
+                            aria-label={data.label} />
+                </a>
+                <text>{data.label}</text>
             </g>
             ))}
         </svg>
