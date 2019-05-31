e45d36318387591d955b879e1c9364b0a4b79a43
Thomas Parisot <oncletom@users.noreply.github.com>
2019-05-31 12:56:15 +0200

    fix: warning React sur les `key` des `LegendList` et `Breadcrumb`

--- src/shared/js/components/LegendList.js
+++ src/shared/js/components/LegendList.js
@@ -3,7 +3,7 @@ import React from 'react';
 export default function({items}){
     return <ol className="legend-list">
         {items.map(({className, url, text, colorClassName}) => {
-            return <li className={className}>
+            return <li key={colorClassName} className={className}>
                 <a href={url} className={url ? 'link' : 'disabled'}>
                     <span className={`color ${colorClassName}`}></span>
                     {text}
