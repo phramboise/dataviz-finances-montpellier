c6ec799601a8a0f8b2f3ff954e219b6c965d430d
Thomas Parisot <oncletom@users.noreply.github.com>
2019-06-28 14:27:49 +0200

    Sélectionne les éléments au survol/focus

--- src/shared/js/components/LegendList.js
+++ src/shared/js/components/LegendList.js
@@ -1,10 +1,14 @@
 import React from 'react';
 
-export default function LegendList({items}){
+export default function LegendList({items, onElementFocus}){
     return <ol className="legend-list">
-        {items.map(({className, url, text, colorClassName}) => {
-            return <li key={colorClassName} className={className}>
-                <a href={url} className={url ? 'link' : 'disabled'}>
+        {items.map(({className, url, text, colorClassName, ariaCurrent}, i) => {
+            return <li key={colorClassName} className={className} aria-current={ariaCurrent}>
+                <a href={url} className={url ? 'link' : 'disabled'} tabIndex="0"
+                    onMouseOver={() => onElementFocus(i)}
+                    onMouseOut={() => onElementFocus(undefined)}
+                    onFocus={() => onElementFocus(i)}
+                    onBlur={() => onElementFocus(undefined)}>
                     <span className={`color ${colorClassName}`}></span>
                     {text}
                 </a>
