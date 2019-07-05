77c54991b47d647f13ca888e0db0c9bd3c888ef4
Thomas Parisot <oncletom@users.noreply.github.com>
2019-07-05 14:58:14 +0200

    Stackchart vers du JSX

--- src/shared/js/components/StackChart.js
+++ src/shared/js/components/StackChart.js
@@ -61,177 +61,147 @@ export default function StackChart ({
 
     const ticks = yScale.ticks(5);
 
-    return React.createElement('div', {
-        className: ['stackchart', portrait ? 'portrait' : ''].filter(e => e).join(' ')
-    },
-    // useless <div> to defend the <svg> in Chrome when using flex: 1 on the legend
-    React.createElement('div', {className: 'over-time'},
-        React.createElement('svg', {viewBox: `0 0 ${WIDTH} ${HEIGHT}`},
-            // x axis
-            React.createElement(D3Axis, {
-                className: 'x',
-                tickData: xs.map(x => {
-                    return {
-                        transform: portrait ?
-                            `translate(0, ${xScale(x)})` :
-                            `translate(${xScale(x)}, ${HEIGHT-HEIGHT_PADDING})`,
-                        line: { x1 : 0, y1 : 0, x2 : 0, y2 : 0 },
-                        text: {
-                            x: portrait ? 0 : -10,
-                            y: 0,
-                            dx: portrait ? '1em' : undefined,
-                            dy: portrait ? undefined : '2em',
-                            anchor: portrait ? 'left' : undefined,
-                            t: x
-                        },
-                        id: x,
-                        className: x === selectedX ? 'selected' : undefined
-                    }
-                }),
-                onSelectedAxisItem: onSelectedXAxisItem
-            }),
-            // y axis
-            React.createElement(D3Axis, {className: 'y', tickData: ticks.map(tick => {
-                return {
-                    transform: portrait ?
-                        `translate(${yScale(tick)}, 0)` :
-                        `translate(0, ${yScale(tick)})`,
-                    line: {
-                        x1 : 0,
-                        y1 : 0,
-                        x2 : portrait ? 0 : WIDTH,
-                        y2 : portrait ? HEIGHT : 0
-                    },
-                    text: {
-                        x: 0,
-                        y: portrait ? 0 : -10,
-                        dx: portrait ? 5 : 0,
-                        dy: portrait ? 12 : 0,
-                        anchor: portrait ? 'left' : 'right',
-                        t: yValueDisplay(tick)
-                    }
+    const xAxisTickData = xs.map(x => ({
+        transform: portrait ?
+            `translate(0, ${xScale(x)})` :
+            `translate(${xScale(x)}, ${HEIGHT-HEIGHT_PADDING})`,
+        line: { x1 : 0, y1 : 0, x2 : 0, y2 : 0 },
+        text: {
+            x: portrait ? 0 : -10,
+            y: 0,
+            dx: portrait ? '1em' : undefined,
+            dy: portrait ? undefined : '2em',
+            anchor: portrait ? 'left' : undefined,
+            t: x
+        },
+        id: x,
+        className: x === selectedX ? 'selected' : undefined
+    }))
 
-                }
-            })}),
-            // content
-            React.createElement('g', {className: `content ${focusedItem !== undefined ? ' with-focus' : ''}`},
-                ysByX.entrySeq().toJS().map(([x, ys]) => {
+    const yAxisTickData = ticks.map(tick => {
+        return {
+            transform: portrait ?
+                `translate(${yScale(tick)}, 0)` :
+                `translate(0, ${yScale(tick)})`,
+            line: {
+                x1 : 0,
+                y1 : 0,
+                x2 : portrait ? 0 : WIDTH,
+                y2 : portrait ? HEIGHT : 0
+            },
+            text: {
+                x: 0,
+                y: portrait ? 0 : -10,
+                dx: portrait ? 5 : 0,
+                dy: portrait ? 12 : 0,
+                anchor: portrait ? 'left' : 'right',
+                t: yValueDisplay(tick)
+            }
 
-                    const total = sum(ys);
+        }
+    })
 
-                    const stackYs = ys
-                    // .map + .slice is an 0(n²) algorithm. Fine here because n is never higher than 20
-                        .map( (amount, i, arr) => sum(arr.slice(0, i)) )
-                        .map(yValueScale)
-                        .map(height => height < 1 ? 0 : height);
+    return (<div className={['stackchart', portrait ? 'portrait' : ''].filter(e => e).join(' ')}>
+        {/* useless <div> to defend the <svg> in Chrome when using flex: 1 on the legend */}
+        <div className="over-time">
+            <svg viewBox={`0 0 ${WIDTH} ${HEIGHT}`}>
+                <D3Axis className="x" tickData={xAxisTickData} onSelectedAxisItem={onSelectedXAxisItem} />
+                <D3Axis className="y" tickData={yAxisTickData} />
+                <g className={`content ${focusedItem !== undefined ? ' with-focus' : ''}`}>
+                    {ysByX.entrySeq().toJS().map(([x, ys]) => {
+                        const total = sum(ys);
 
-                    const stack = ys
-                        .map((y, i) => {
-                            const baseHeight = yValueScale(y);
+                        // .map + .slice is an 0(n²) algorithm. Fine here because n is never higher than 20
+                        const stackYs = ys
+                            .map( (amount, i, arr) => sum(arr.slice(0, i)) )
+                            .map(yValueScale)
+                            .map(height => height < 1 ? 0 : height);
 
-                            const height = baseHeight >= 1 ?
-                                Math.max(baseHeight - BRICK_SPACING, MIN_BRICK_HEIGHT) :
-                                0;
+                        const stack = ys
+                            .map((y, i) => {
+                                const baseHeight = yValueScale(y);
 
-                            const baseY = portrait ?
-                                +BRICK_SPACING/2 :
-                                HEIGHT - HEIGHT_PADDING - height - BRICK_SPACING/2;
+                                const height = baseHeight >= 1 ?
+                                    Math.max(baseHeight - BRICK_SPACING, MIN_BRICK_HEIGHT) :
+                                    0;
 
-                            return {
-                                value: y,
-                                height,
-                                y: i === 0 ?
-                                    baseY :
-                                    (portrait ?
-                                        baseY + stackYs[i] :
-                                        baseY - stackYs[i])
-                            }
-                        });
+                                const baseY = portrait ?
+                                    +BRICK_SPACING/2 :
+                                    HEIGHT - HEIGHT_PADDING - height - BRICK_SPACING/2;
+
+                                return {
+                                    value: y,
+                                    height,
+                                    y: i === 0 ?
+                                        baseY :
+                                        (portrait ?
+                                            baseY + stackYs[i] :
+                                            baseY - stackYs[i])
+                                }
+                            });
 
 
-                    const totalHeight = yValueScale(total);
+                        const totalHeight = yValueScale(total);
 
-                    const totalY = HEIGHT - HEIGHT_PADDING - totalHeight;
+                        const totalY = HEIGHT - HEIGHT_PADDING - totalHeight;
 
-                    return React.createElement('g',
-                        {
-                            transform: portrait ? `translate(0, ${xScale(x) + 6 })` : `translate(${xScale(x)})`,
-                            key: x,
-                            className: x === selectedX ? 'selected' : ''
-                        },
-                        React.createElement('g', {},
-
-                            stack.map( ({value, height, y}, i) => {
+                        return (<g key={x} className={x === selectedX ? 'selected' : ''} transform={portrait ? `translate(0, ${xScale(x) + 6 })` : `translate(${xScale(x)})`}>
+                            <g>{stack.map( ({value, height, y}, i) => {
                                 const legendItem = legendItems[i] || {}
                                 const colorClass = legendItem.colorClassName || uniqueColorClass;
 
-                                return React.createElement(
-                                    'g',
-                                    {
-                                        transform: portrait ? `translate(${y})` : `translate(0, ${y})`,
-                                        className: [
-                                            'brick',
-                                            onBrickClicked ? 'actionable' : '',
-                                            colorClass,
-                                            legendItems.length ? legendItems[i].id : contentId,
-                                            focusedItem === i ? 'focused' : ''
-                                        ].join(' '),
-                                        key: i,
-                                        onClick: onBrickClicked ? () => {
-                                            onBrickClicked(
-                                                x,
-                                                legendItems ? legendItems[i].id : y
-                                            )
-                                        } : undefined,
-                                        onMouseOver: () => setFocusedItem(i),
-                                        onFocus: () => setFocusedItem(i),
-                                        onMouseOut: () => setFocusedItem(undefined),
-                                        onBlur: () => setFocusedItem(undefined),
-                                    },
-                                    React.createElement('rect', {
-                                        x: portrait ? 0 : -columnWidth/2,
-                                        y: 0,
-                                        width: portrait ? height : columnWidth,
-                                        height: portrait ? PORTRAIT_COLUMN_WIDTH - 12 : height,
-                                        rx: BRICK_RADIUS,
-                                        ry: BRICK_RADIUS
-                                    }),
-                                    (stack.length >= 2 && BRICK_DISPLAY_VALUE && (
-                                        (!portrait && height >= 30) ||
-                                            (portrait && height >= 60)
-                                    )) ? React.createElement('text', {
-                                            transform: portrait ?
-                                                `translate(2, 20)`:
-                                                `translate(-${columnWidth/2 - 10}, 20)`
-                                        }, yValueDisplay(value)) : undefined
-                                )
-                            })
-                        ),
-                        React.createElement(
-                            'text',
-                            {
-                                className: 'stackchart-title',
-                                x: portrait ? WIDTH - 90 : -columnWidth/2,
-                                y: portrait ? 0 : totalY,
-                                dy: portrait ? '-6' : '-1em',
-                                dx: '0em',
-                                textAnchor: 'right'
-                            },
-                            React.createElement('tspan', {}, yValueDisplay(total))
-                        )
-                    )
-                })
-            )
-        )
-    ),
-    legendItems ? React.createElement(LegendList, {
-        items: legendItems.map((li, i) => Object.assign(
-            {colorClassName: `area-color-${i+1}`},
-            {ariaCurrent: focusedItem === i},
-            li,
-            {className: `${li.className} ${focusedItem === i ? 'focused': ''}`},
-        )),
-        onElementFocus: (i) => setFocusedItem(i),
-    }) : undefined
-    )
+                                return (<g key={i}
+                                            transform={portrait ? `translate(${y})` : `translate(0, ${y})`}
+                                            className={[
+                                                'brick',
+                                                onBrickClicked ? 'actionable' : '',
+                                                colorClass,
+                                                legendItems.length ? legendItems[i].id : contentId,
+                                                focusedItem === i ? 'focused' : ''
+                                            ].join(' ')}
+                                            onClick={onBrickClicked ? () => {
+                                                onBrickClicked(
+                                                    x,
+                                                    legendItems ? legendItems[i].id : y
+                                                )
+                                            } : undefined}
+                                            onMouseOver={() => setFocusedItem(i)}
+                                            onFocus={() => setFocusedItem(i)}
+                                            onMouseOut={() => setFocusedItem(undefined)}
+                                            onBlur={() => setFocusedItem(undefined)}
+                                    >
+                                        <rect y={0} x={portrait ? 0 : -columnWidth/2}
+                                            width={portrait ? height : columnWidth}
+                                            height={portrait ? PORTRAIT_COLUMN_WIDTH - 12 : height}
+                                            rx={BRICK_RADIUS}
+                                            ry={BRICK_RADIUS}>
+                                            <text transform={portrait ? `translate(2, 20)` : `translate(-${columnWidth/2 - 10}, 20)`}
+                                                    hidden={!(stack.length >= 2 && BRICK_DISPLAY_VALUE && ((!portrait && height >= 30) || (portrait && height >= 60)))}>
+                                                {yValueDisplay(value)}
+                                            </text>
+                                        </rect>
+                                    </g>)
+                            })}
+                            <text className="stackchart-title"
+                                    x={portrait ? WIDTH - 90 : -columnWidth/2}
+                                    y={portrait ? 0 : totalY}
+                                    dy={portrait ? '-6' : '-1em'}
+                                    dx="0em"
+                                    textAnchor="right">
+                                <tspan>{yValueDisplay(total)}</tspan>
+                            </text>
+                        </g>
+                    </g>)
+                    })}
+                </g>
+            </svg>
+        </div>
+        {legendItems && <LegendList onElementFocus={(i) => setFocusedItem(i)}
+            items={legendItems.map((li, i) => Object.assign(
+                {colorClassName: `area-color-${i+1}`},
+                {ariaCurrent: focusedItem === i},
+                li,
+                {className: `${li.className} ${focusedItem === i ? 'focused': ''}`},
+            ))} />}
+    </div>);
 }
