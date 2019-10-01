068c7e4ea048a575ac26dd69005f59f02d6187b8
Thomas Parisot <oncletom@users.noreply.github.com>
2019-10-01 16:00:09 +0200

    Détails : Sous-totaux par Politique/Sous-Politique
    fix #155

--- src/shared/js/components/DetailsTable.js
+++ src/shared/js/components/DetailsTable.js
@@ -2,9 +2,8 @@ import React, {Fragment} from 'react';
 
 import {makeLigneBudgetId}  from 'document-budgetaire/Records.js';
 
-import {fonctionLabels, natureLabels} from '../../../../build/finances/finance-strings.json';
-
-import {currencyFormat, ScaledAmount, default as MoneyAmount} from './MoneyAmount';
+import {natureLabels} from '../../../../build/finances/finance-strings.json';
+import {currencyFormat, percentage} from './MoneyAmount.js';
 
 const SPACE_REGEX = new RegExp(' ', 'g');
 const DOT_REGEX = new RegExp('\\.', 'g');
@@ -20,36 +19,42 @@ const id = (string) => string.replace(PRE_DOT, '').replace(SPACE_REGEX, '-').rep
 export default function DetailsTable({families, politiqueId}) {
     return <table className="raw-data">
         <tbody>
-        {families.filter(byPolitique(politiqueId)).map(Politique => {
-            return <Fragment key={Politique.id}>
-            <tr className="colgroup colgroup--group">
-                <th id={id(Politique.id)} colSpan="2" scope="colgroup">{Politique.label}</th>
-            </tr>
-
-            {Politique.children.map(SousPolitique => <Fragment key={SousPolitique.id}>
-                <tr className="colgroup colgroup--subgroup">
-                    <th id={id(SousPolitique.id)} colSpan="2" scope="colgroup">
-                        {SousPolitique.label}
-                    </th>
-                </tr>
-                <tr>
-                    <th id="raw-col-nature" scope="col">Nature</th>
-                    <th id="raw-col-montant" scope="col" className="digits">Montant</th>
-                </tr>
-                {SousPolitique.elements.sort(byAmount).map(ligne => (
-                    <tr key={makeLigneBudgetId(ligne)} data-fonction-nature={makeLigneBudgetId(ligne)} className="raw-record" tabIndex="0">
-                        <td headers={`${id(Politique.id)} ${id(SousPolitique.id)} raw-col-nature`}>
-                            {natureLabels[ ligne['Nature'] ]}
-                        </td>
-                        <td headers={`${id(Politique.id)} ${id(SousPolitique.id)} raw-col-montant`} className="money-amount">
-                            {currencyFormat(ligne['MtReal'])}
-                        </td>
+            {families.filter(byPolitique(politiqueId)).map(Politique => {
+                return <Fragment key={Politique.id}>
+                    <tr className="colgroup colgroup--group">
+                        <th id={id(Politique.id)} scope="colgroup">{Politique.label}</th>
+                        <th className="money-amount" scope="colgroup" aria-label={`Montant total : ${currencyFormat(Politique.total)}`}>
+                            {currencyFormat(Politique.total)}
+                        </th>
                     </tr>
-                ))}
+
+                    {Politique.children.map(SousPolitique => <Fragment key={SousPolitique.id}>
+                        <tr className="colgroup colgroup--subgroup">
+                            <th id={id(SousPolitique.id)} scope="colgroup">
+                                {SousPolitique.label}
+                            </th>
+                            <th className="money-amount" scope="colgroup" aria-label={`Montant total : ${currencyFormat(SousPolitique.total)}`}>
+                                {currencyFormat(SousPolitique.total)}
+                            </th>
+                        </tr>
+                        <tr>
+                            <th id="raw-col-nature" scope="col">Nature</th>
+                            <th id="raw-col-montant" scope="col" className="digits">Montant</th>
+                        </tr>
+                        {SousPolitique.elements.sort(byAmount).map(ligne => (
+                            <tr key={makeLigneBudgetId(ligne)} data-fonction-nature={makeLigneBudgetId(ligne)} className="raw-record" tabIndex="0">
+                                <td headers={`${id(Politique.id)} ${id(SousPolitique.id)} raw-col-nature`}>
+                                    {natureLabels[ ligne['Nature'] ]}
+                                </td>
+                                <td headers={`${id(Politique.id)} ${id(SousPolitique.id)} raw-col-montant`} className="money-amount">
+                                    {currencyFormat(ligne['MtReal'])}
+                                </td>
+                            </tr>
+                        ))}
+                    </Fragment>
+                    )}
                 </Fragment>
-            )}
-            </Fragment>
-        })}
+            })}
         </tbody>
     </table>
 }
