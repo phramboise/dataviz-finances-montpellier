6a4235611c353c7efc68a4946956a34c2191cbfe
Thomas Parisot <oncletom@users.noreply.github.com>
2019-11-06 17:12:23 +0100

    Fil d'ariane

--- src/shared/js/components/Breadcrumb.js
+++ src/shared/js/components/Breadcrumb.js
@@ -1,9 +1,9 @@
 import React from 'react';
 
 export default function Breadcrumb ({ items }) {
-    return <nav aria-label="Breadcrumb">
-        <ol>{items.map(({url, text}) => <li key={url+text}>
+    return <nav class="filAriane" aria-label="Breadcrumb">
+        <ul>{items.map(({url, text}) => <li key={url+text}>
             <a href={url ? url : '#'} rel="permalink" aria-current={url ? false : 'page'}>{text}</a>
-        </li>)}</ol>
+        </li>)}</ul>
     </nav>;
 }
