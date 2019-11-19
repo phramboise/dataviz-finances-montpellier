2044c36bb10f4803002e8853ad2c98bff9408632
Thomas Parisot <oncletom@users.noreply.github.com>
2019-11-19 09:32:28 +0100

    Fix la double imbrication de .filAriane

--- src/shared/js/components/Breadcrumb.js
+++ src/shared/js/components/Breadcrumb.js
@@ -1,9 +1,9 @@
 import React from 'react';
 
 export default function Breadcrumb ({ items }) {
-    return <nav class="filAriane" aria-label="Breadcrumb">
-        <ul>{items.map(({url, text}) => <li key={url+text}>
+    return <ul aria-label="Breadcrumb">
+        {items.map(({url, text}) => <li key={url+text}>
             <a href={url ? url : '#'} rel="permalink" aria-current={url ? false : 'page'}>{text}</a>
-        </li>)}</ul>
-    </nav>;
+        </li>)}
+    </ul>;
 }
